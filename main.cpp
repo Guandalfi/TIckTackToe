f6cd52b94121b36b2f97b440a4d5d52a516f8ba6
Lucas Guandalin <106684353+Guandalfi@users.noreply.github.com>
2023-10-19 00:36:24 -0300

    Update main.cpp
    Adicionado função para verificar empate.
    Começado lógica para verificar vencedor.
    Alterado forma de verificar se a posição já possui jogada para não utilizar a keyword 'goto'.

--- main.cpp
+++ main.cpp
@@ -26,6 +26,48 @@ int display_game_grid(int game_grid[3][3])
 }
 
 
+bool is_draw(int game_grid[3][3])
+{
+    int places_taken = 0;
+
+    for (size_t i = 0; i < 3; i++)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+            if (game_grid[i][j] != 0)
+            {
+                places_taken += 1;
+            }
+            if (places_taken == 9)
+            {
+                return true;
+            }
+            
+        }
+        
+    }
+    return false;
+}
+
+
+bool is_winner(int game_grid[3][3])
+{
+    for (size_t i = 0; i < 3; i++)
+    {
+        int rows[3];
+        for (size_t j = 0; j < 3; j++)
+        {
+            rows[j] = game_grid[i][j];
+        }
+        for (size_t i = 0; i < 3; i++)
+    {
+        std::cout << rows[i];
+    } 
+    } 
+    return true;   
+}
+
+
 int main(int argc, char const *argv[])
 {   
     int game_grid[3][3] = {
@@ -43,9 +85,17 @@ int main(int argc, char const *argv[])
     {
         
         display_game_grid(game_grid);
+
+        if (is_draw(game_grid))
+        {
+            std::cout << "It's a draw !";
+            return 0;
+        }
         
+        is_winner(game_grid); 
+
         int change; // 1 = 'X'; 2 = 'O'
-        int grid_position;
+        int grid_position; // player position input
         
         if (player_x)
         {
@@ -59,16 +109,25 @@ int main(int argc, char const *argv[])
             change = 2;
         }
 
-        if (grid_position > 9)
+        int valid_inputs[9] = {1,2,3,4,5,6,7,8,9}; //verify if player input is valid
+        bool valid_input = false;
+        for (size_t i = 0; i < 9; i++)
+        {
+            if (grid_position == valid_inputs[i])
+            {
+                valid_input = true;
+                break;
+            } 
+        }
+        if (!valid_input) //if player input not find in valid_inputs
         {
             system("cls");
             std::cout << "Digite uma posicao entre 1 e 9 !!!" << std::endl;
             continue;
         }
         
-        //int total_elements = sizeof(game_grid) / sizeof(game_grid[0][0]);
-        
         int couter = 1;
+        bool position_occupied = false; //verify if grid_position is already occupied
         for (size_t i = 0; i < 3; i++)
         {
             for (size_t j = 0; j < 3; j++)
@@ -76,16 +135,18 @@ int main(int argc, char const *argv[])
                 if (couter == grid_position){
                     if (game_grid[i][j] == 0)
                     {
-                        game_grid[i][j] = change;                        
-                    }else
-                    {
-                        system("cls");
-                        std::cout << "posicao ja foi jogada" << std::endl;
-                        goto try_again;
-                    }
+                        game_grid[i][j] = change;
+                        position_occupied = true;
+                    }                    
                 }
                 couter++;               
             }
+        }
+        if (!position_occupied)
+        {        
+            system("cls");
+            std::cout << "posicao ja foi jogada" << std::endl;
+            continue;
         }
 
         if (player_x)
@@ -98,9 +159,6 @@ int main(int argc, char const *argv[])
             player_x = true;            
         }
         system("cls");
-        try_again:
-            continue;
     }
     return 0;
 }
-
